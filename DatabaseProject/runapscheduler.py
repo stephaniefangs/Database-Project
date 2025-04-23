a80869e670efcaeaf892c81386d3c217342a7b8e
Sam Colvin <hmc3cx@virginia.edu>
2025-04-22 21:29:56 -0400

    migrated from crontab to apscheduler

---
+++ DatabaseProject/runapscheduler.py
@@ -0,0 +1,82 @@
+# https://pypi.org/project/django-apscheduler/
+import logging
+
+from django.conf import settings
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django.core.management.base import BaseCommand
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
+from django_apscheduler import util
+from django.db import connection
+from .models import Reservations
+from django.db import connection
+
+logger = logging.getLogger(__name__)
+
+
+def add_fees():
+    overdue_books = Reservations.objects.raw("SELECT * FROM reservations WHERE CURRENT_DATE > reservations.due_date AND reservations.return_date IS NULL")
+    with connection.cursor() as cursor:
+        for book in overdue_books:
+            cursor.execute("""
+                UPDATE users
+                SET outstanding_balance = outstanding_balance + 0.25
+                WHERE users.user_id = %s                  
+            """, [book.user_id])
+    return
+
+
+# The `close_old_connections` decorator ensures that database connections, that have become
+# unusable or are obsolete, are closed before and after your job has run. You should use it
+# to wrap any jobs that you schedule that access the Django database in any way. 
+@util.close_old_connections
+def delete_old_job_executions(max_age=604_800):
+  """
+  This job deletes APScheduler job execution entries older than `max_age` from the database.
+  It helps to prevent the database from filling up with old historical records that are no
+  longer useful.
+  
+  :param max_age: The maximum length of time to retain historical job execution records.
+                  Defaults to 7 days.
+  """
+  DjangoJobExecution.objects.delete_old_job_executions(max_age)
+
+
+class Command(BaseCommand):
+  help = "Runs APScheduler."
+
+  def handle(self, *args, **options):
+    scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+    scheduler.add_jobstore(DjangoJobStore(), "default")
+
+    scheduler.add_job(
+      add_fees,
+      trigger=CronTrigger(hour="00", minute="00"),
+      id="add_fees",
+      max_instances=1,
+      replace_existing=True,
+    )
+    logger.info("Added job 'add_fees'.")
+
+    scheduler.add_job(
+      delete_old_job_executions,
+      trigger=CronTrigger(
+        day_of_week="mon", hour="00", minute="00"
+      ),  # Midnight on Monday, before start of the next work week.
+      id="delete_old_job_executions",
+      max_instances=1,
+      replace_existing=True,
+    )
+    logger.info(
+      "Added weekly job: 'delete_old_job_executions'."
+    )
+
+    try:
+      logger.info("Starting scheduler...")
+      scheduler.start()
+    except KeyboardInterrupt:
+      logger.info("Stopping scheduler...")
+      scheduler.shutdown()
+      logger.info("Scheduler shut down successfully!")
