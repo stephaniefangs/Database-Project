6f9b9bb1601906d0bf06b728c7099a82370bf567
Sam Colvin <hmc3cx@virginia.edu>
2025-04-22 21:03:28 -0400

    re-formatted nested loop in scheduled function

--- DatabaseProject/cron.py
+++ DatabaseProject/cron.py
@@ -4,11 +4,11 @@ from django.db import connection
 
 def increment_fees():
     overdue_books = Reservations.objects.raw("SELECT * FROM reservations WHERE CURRENT_DATE > reservations.due_date")
-    for book in overdue_books:
-        with connection.cursor() as cursor:
-                cursor.execute("""
-                    UPDATE users
-                    SET outstanding_balance = outstanding_balance + 0.25
-                    WHERE users.user_id = %s                  
-                """, [book.user_id])
+    with connection.cursor() as cursor:
+        for book in overdue_books:
+            cursor.execute("""
+                UPDATE users
+                SET outstanding_balance = outstanding_balance + 0.25
+                WHERE users.user_id = %s                  
+            """, [book.user_id])
     return
